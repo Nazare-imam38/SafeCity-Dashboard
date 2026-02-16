f53272aa2b3504535e6cced94e06dbfa7f827533
sana-ali18 <sana.znz18@gmail.com>
2026-02-16 14:54:10 +0500

    models, secrets, base.py, local.py and production.,py

---
+++ SafeCity-Backend/api/models.py
@@ -0,0 +1,125 @@
+from django.db import models
+from django.utils import timezone 
+from django.contrib.auth.base_user import AbstractBaseUser,BaseUserManager
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.core.validators import FileExtensionValidator
+# from .utils import project_doc_file_path, project_image_file_path
+from dateutil.relativedelta import relativedelta
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, company_name, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            company_name=company_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, company_name, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            company_name=company_name,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+    
+class Stakeholder(models.Model):
+    # TYPE_CHOICES = (
+    #     ('Client', 'Client'),
+    #     ('Consultant', 'Consultant'),
+    #     ('Contractor', 'Contractor'),
+    #     ('Subcontractor', 'Subcontractor'),
+    #     ('Supplier', 'Supplier'),
+    #     ('Regulatory_Authority', 'Regulatory_Authority'),
+    #     ('Other', 'Other'),
+    # )
+    # stakeholder_type = models.CharField(max_length=255, choices=TYPE_CHOICES)
+    stakeholder_type = models.CharField(max_length=255)
+    stakeholder_title = models.CharField(max_length=255)
+    status_choices = (
+        ('active', 'Active'),
+        ('disable', 'Disable'),
+    )
+    status = models.CharField(max_length=255, choices=status_choices, default='active')
+
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+    )
+    stakeholder = models.ForeignKey(Stakeholder, on_delete=models.DO_NOTHING, null=True, blank=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=255)
+    # companyLogo  = models.URLField(null=True,blank=True)
+    companyLogo = models.ImageField(
+        upload_to=project_image_file_path,
+        null=True,
+        blank=True
+    )
+    is_active = models.BooleanField(default=True)
+    country = models.CharField(max_length=200, null=True, blank=True)
+    address = models.CharField(max_length=400, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    zipcode = models.CharField(max_length=200, null=True, blank=True)
+    is_admin = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    profileImage  =  models.ImageField(
+        upload_to=project_image_file_path,
+        null=True,
+        blank=True
+    )
+    
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['company_name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+    @property
+    def get_full_name(self):
+        '''
+        Returns the first_name plus the last_name, with a space in between.
+        '''
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
